65045ef51cc3726b54da44e0a0849992e6944bea
Kathryn Baker <k.baker@stfc.ac.uk>
2023-05-24 09:40:12 +0100

    ensemble shell script and experiment IDs for mlflow

---
+++ compare_ensembles.sh
@@ -0,0 +1,6 @@
+python experiments/ensemble.py --epochs 1000 --data_source variational_random_wo_constants --learning_rate 1e-5 --batch_size 64 --experiment_name variational_learning
+python experiments/ensemble.py --epochs 5000 --data_source variational_random_wo_constants --learning_rate 1e-5 --batch_size 64 --experiment_name variational_learning
+python experiments/ensemble.py --epochs 10000 --data_source variational_random_wo_constants --learning_rate 1e-5 --batch_size 64 --experiment_name variational_learning
+python experiments/ensemble.py --epochs 1000 --data_source variational_random_wo_constants --learning_rate 1e-4 --batch_size 64 --experiment_name variational_learning
+python experiments/ensemble.py --epochs 5000 --data_source variational_random_wo_constants --learning_rate 1e-4 --batch_size 64 --experiment_name variational_learning
+python experiments/ensemble.py --epochs 10000 --data_source variational_random_wo_constants --learning_rate 1e-4 --batch_size 64 --experiment_name variational_learning
